550ce8adfdc5a9696927bfda6d108995bab59cd7
mekows <1361394078@qq.com>
2018-06-01 23:53:24 +0800

    xml转json工具包

---
+++ dao/src/main/java/com/mekows/projectv1/dao/ListMap.java
@@ -0,0 +1,14 @@
+package com.mekows.projectv1.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ListMap {
+    public static void main(String[] args){
+        List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+        List<Map> list2= new ArrayList<Map>();
+        Map<String,Object> map = new HashMap<String, Object>();
+    }
+}
